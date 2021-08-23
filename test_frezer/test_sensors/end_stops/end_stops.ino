d6094bf15a38d4ea13a732d58521ef315534d8d4
honepa <cot1884@gmail.com>
2021-08-23 19:54:00 +0500

    end stops

--- test_frezer/test_sensors/end_stops/end_stops.ino
+++ test_frezer/test_sensors/end_stops/end_stops.ino
@@ -9,11 +9,30 @@ void setup()
 
 void loop()
 {
+  /*
   Serial.print(digitalRead(19)); //y
   Serial.print("  ");
   Serial.print(digitalRead(15)); //x
   Serial.print("  ");
   Serial.print(digitalRead(2)); //z
   Serial.println("");
-  delay(1500);
+  //delay(1500);
+  */
+  int y, x, z = 0;
+  y = digitalRead(19);
+  x = digitalRead(15);
+  z = digitalRead(2);
+  if(!y)
+  {
+    Serial.println("Y");
+  }
+  else if(!x)
+  {
+    Serial.println("X");
+  }
+  else if(!z)
+  {
+    Serial.println("Z");
+  }
+
 }
